7b055927a8283526c215eddc6e19a02d17c1c8b5
BiancaPBM <pantarubianca@yahoo.com>
2020-03-29 22:43:24 +0300

    indenting + small fixeses

--- template.js
+++ template.js
@@ -1,3 +1,12 @@
 redirect = function() {
     window.location.href = "file:///D:/Facultate/Tehnologii%20Web/proiect%20tw/Proiect-TW/index.html";
   };
+
+  var searchResult = document.getElementById("search-txt");
+  var searchValue = searchResult? searchResult.value: []
+
+  function search(context) {
+    if(event.key === 'Enter' && context.value != [])  {
+        alert(context.value);        
+    }
+}

cd8e4e2a4ccd0ae50a072347d8bab2b62d76824f
cristirusu-99 <59122086+cristirusu-99@users.noreply.github.com>
2020-04-12 17:53:15 +0300

    Update serverDestept.js

--- SERVER/serverDestept.js
+++ SERVER/serverDestept.js
@@ -2,40 +2,68 @@
 // there is a special variable in each module __dirname it contains the directory name of this file
 
 var http = require('http');
-var url = require('url');
 var fs = require('fs');
+var path = require('path');
+
+const deniedPath = /\.\.\//
 
 http.createServer(function (request, response) {
-console.log(request.method);
- let identiifiers =   request.url.split('/');
-    console.log(identiifiers);
+    console.log('request ', request.url);
 
-    fs.readFile('./' + request.url, function (err, data) {
-        if (!err) {
-            var dotoffset = request.url.lastIndexOf('.');
-            var mimetype = dotoffset == -1
-                ? 'text/plain'
-                : {
-                    '.html': 'text/html',
-                    '.ico': 'image/x-icon',
-                    '.jpg': 'image/jpeg',
-                    '.png': 'image/png',
-                    '.gif': 'image/gif',
-                    '.css': 'text/css',
-                    '.js': 'text/javascript',
-                    '.json': 'application/json'
-                }[request.url.substr(dotoffset)];
-            response.setHeader('Content-type', mimetype);
-            response.end(data);
-            console.log(request.url, mimetype);
-        } else {
-            console.log('file not found: ' + request.url);
-            //   response.writeHead(404, "Not Found");
-            response.end();
-        }
-    });
-}
-).listen(3000);
+    var filePath = '.' + request.url;
+    if (filePath == './') {
+        filePath = './index.html';
+    }
+
+    var forbiddenPath = filePath.match(deniedPath);
+
+    if (forbiddenPath != null) {
+        fs.readFile('./403.html', function (error, content) {
+            response.writeHead(403, { 'Content-Type': 'text/html' });
+            response.end(content, 'utf-8');
+        });
+    }
+    else {
+        var extname = String(path.extname(filePath)).toLowerCase();
+        var mimeTypes = {
+            '.html': 'text/html',
+            '.js': 'text/javascript',
+            '.css': 'text/css',
+            '.json': 'application/json',
+            '.png': 'image/png',
+            '.jpg': 'image/jpg',
+            '.gif': 'image/gif',
+            '.svg': 'image/svg+xml',
+            '.wav': 'audio/wav',
+            '.mp4': 'video/mp4',
+            '.woff': 'application/font-woff',
+            '.ttf': 'application/font-ttf',
+            '.eot': 'application/vnd.ms-fontobject',
+            '.otf': 'application/font-otf',
+            '.wasm': 'application/wasm'
+        };
+
+        var contentType = mimeTypes[extname] || 'application/octet-stream';
+
+        fs.readFile(filePath, function (error, content) {
+            if (error) {
+                if (error.code == 'ENOENT') {
+                    fs.readFile('./404.html', function (error, content) {
+                        response.writeHead(404, { 'Content-Type': 'text/html' });
+                        response.end(content, 'utf-8');
+                    });
+                }
+                else {
+                    response.writeHead(500);
+                    response.end('Sorry, check with the site admin for error: ' + error.code + ' ..\n');
+                }
+            }
+            else {
+                response.writeHead(200, { 'Content-Type': contentType });
+                response.end(content, 'utf-8');
+            }
+        });
+    }
+
+}).listen(3000);
 console.log('Server running at http://127.0.0.1:3000/');
-
-
